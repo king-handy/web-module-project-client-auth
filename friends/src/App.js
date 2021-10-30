db38fc316b686f9a7c8140c637d60eb3b65fae63
Stephen Handy <k1nghandy@MacBook-Air.fios-router.home>
2021-10-29 23:43:33 -0400

    sign in auth

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,13 @@
 import { connect } from 'react-redux';
 import './App.css';
+import { signIn } from './services/firebase.service';
 
-function App(props) {
+function App(props) { 
+
   return (
     <div className="App">
       <h1>{props.title}</h1>
+      <button onClick={signIn}>Sign In</button>
     </div>
   );
 }
