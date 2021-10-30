3bdd1c5de8d827b0941985834a2a52c503d4d07c
Stephen Handy <k1nghandy@MacBook-Air.fios-router.home>
2021-10-29 22:00:53 -0400

    initial state

--- friends/src/reducer/index.js
+++ friends/src/reducer/index.js
@@ -1,10 +1,13 @@
 import { combineReducers, createStore } from "redux"
 
 const initialState = {
-    title: 'friends'
+    id: 1,
+    name: 'Joe',
+    age: 24,
+    email: 'joe@lambdaschool.com'
 }
 
-const sharedReducer = (state, action) => {
+const sharedReducer = (state = initialState, action) => {
     switch(action.type) {
         default:
             return state
